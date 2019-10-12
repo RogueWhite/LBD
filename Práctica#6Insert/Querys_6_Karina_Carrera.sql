ddcd690d83f1ae57fa98cc2efcae01d3463f1297
RogueWhite <54918091+RogueWhite@users.noreply.github.com>
2019-10-11 22:58:24 -0500

    Add files via upload

---
+++ Práctica#6Insert/Querys_6_Karina_Carrera.sql
@@ -0,0 +1,256 @@
+Select * from Municipios
+UPDATE Municipios SET Municipio_ID = 114 WHERE Municipio_ID = 115
+UPDATE Municipios SET Municipio_ID = 115 WHERE Municipio_ID = 116
+UPDATE Municipios SET Municipio_ID = 116 WHERE Municipio_ID = 117
+UPDATE Municipios SET Municipio_ID = 117 WHERE Municipio_ID = 118
+UPDATE Municipios SET Municipio_ID = 118 WHERE Municipio_ID = 119
+
+INSERT INTO Municipios values (1,'Acacoyagua
+', 5)
+INSERT INTO Municipios values (2,'Acala
+', 5)
+INSERT INTO Municipios values (3,'Acapetahua
+', 5)
+INSERT INTO Municipios values (4,'Aldama
+', 5)
+INSERT INTO Municipios values (5,'Altamirano
+', 5)
+INSERT INTO Municipios values (6,'Amatan
+', 5)
+INSERT INTO Municipios values (7,'Amatenango de la Frontera
+', 5)
+INSERT INTO Municipios values (8,'Amatenango del Valle
+', 5)
+INSERT INTO Municipios values (9,'Angel Albino Corzo
+', 5)
+INSERT INTO Municipios values (10,'Arriaga
+', 5)
+INSERT INTO Municipios values (11,'Bejucal de Ocampo
+', 5)
+INSERT INTO Municipios values (12,'Bella Vista
+', 5)
+INSERT INTO Municipios values (13,'Benemerito de las Americas
+', 5)
+INSERT INTO Municipios values (14,'Berriozabal
+', 5)
+INSERT INTO Municipios values (15,'Bochil
+', 5)
+INSERT INTO Municipios values (16,'Cacahoatan
+', 5)
+INSERT INTO Municipios values (17,'Catazaja
+', 5)
+INSERT INTO Municipios values (18,'Chalchihuitan
+', 5)
+INSERT INTO Municipios values (19,'Chamula
+', 5)
+INSERT INTO Municipios values (20,'Chanal
+', 5)
+INSERT INTO Municipios values (21,'Chapultenango
+', 5)
+INSERT INTO Municipios values (22,'Chenalho
+', 5)
+INSERT INTO Municipios values (23,'Chiapa de Corzo
+', 5)
+INSERT INTO Municipios values (24,'Chiapilla
+', 5)
+INSERT INTO Municipios values (25,'Chicoasen
+', 5)
+INSERT INTO Municipios values (26,'Chicomuselo
+', 5)
+INSERT INTO Municipios values (27,'Chilon
+', 5)
+INSERT INTO Municipios values (28,'Cintalapa
+', 5)
+INSERT INTO Municipios values (29,'Coapilla
+', 5)
+INSERT INTO Municipios values (30,'Comitan de Dominguez
+', 5)
+INSERT INTO Municipios values (31,'Copainala
+', 5)
+INSERT INTO Municipios values (32,'El Bosque
+', 5)
+INSERT INTO Municipios values (33,'El Porvenir
+', 5)
+INSERT INTO Municipios values (34,'Escuintla
+', 5)
+INSERT INTO Municipios values (35,'Francisco Leon
+', 5)
+INSERT INTO Municipios values (36,'Frontera Comalapa
+', 5)
+INSERT INTO Municipios values (37,'Frontera Hidalgo
+', 5)
+INSERT INTO Municipios values (38,'Huehuetan
+', 5)
+INSERT INTO Municipios values (39,'Huitiupan
+', 5)
+INSERT INTO Municipios values (40,'Huixtan
+', 5)
+INSERT INTO Municipios values (41,'Huixtla
+', 5)
+INSERT INTO Municipios values (42,'Ixhuatan
+', 5)
+INSERT INTO Municipios values (43,'Ixtacomitan
+', 5)
+INSERT INTO Municipios values (44,'Ixtapa
+', 5)
+INSERT INTO Municipios values (45,'Ixtapangajoya
+', 5)
+INSERT INTO Municipios values (46,'Jiquipilas
+', 5)
+INSERT INTO Municipios values (47,'Jitotol
+', 5)
+INSERT INTO Municipios values (48,'Juarez
+', 5)
+INSERT INTO Municipios values (49,'La Concordia
+', 5)
+INSERT INTO Municipios values (50,'La Grandeza
+', 5)
+INSERT INTO Municipios values (51,'La Independencia
+', 5)
+INSERT INTO Municipios values (52,'La Libertad
+', 5)
+INSERT INTO Municipios values (53,'La Trinitaria
+', 5)
+INSERT INTO Municipios values (54,'Larrainzar
+', 5)
+INSERT INTO Municipios values (55,'Las Margaritas
+', 5)
+INSERT INTO Municipios values (56,'Las Rosas
+', 5)
+INSERT INTO Municipios values (57,'Mapastepec
+', 5)
+INSERT INTO Municipios values (58,'Maravilla Tenejapa
+', 5)
+INSERT INTO Municipios values (59,'Marques de Comillas
+', 5)
+INSERT INTO Municipios values (60,'Mazapa de Madero
+', 5)
+INSERT INTO Municipios values (61,'Mazatan
+', 5)
+INSERT INTO Municipios values (62,'Metapa
+', 5)
+INSERT INTO Municipios values (63,'Mitontic
+', 5)
+INSERT INTO Municipios values (64,'Montecristo de Guerrero
+', 5)
+INSERT INTO Municipios values (65,'Motozintla
+', 5)
+INSERT INTO Municipios values (66,'Nicolas Ruiz
+', 5)
+INSERT INTO Municipios values (67,'Ocosingo
+', 5)
+INSERT INTO Municipios values (68,'Ocotepec
+', 5)
+INSERT INTO Municipios values (69,'Ocozocoautla de Espinosa
+', 5)
+INSERT INTO Municipios values (70,'Ostuacan
+', 5)
+INSERT INTO Municipios values (71,'Osumacinta
+', 5)
+INSERT INTO Municipios values (72,'Oxchuc
+', 5)
+INSERT INTO Municipios values (73,'Palenque
+', 5)
+INSERT INTO Municipios values (74,'Pantelho
+', 5)
+INSERT INTO Municipios values (75,'Pantepec
+', 5)
+INSERT INTO Municipios values (76,'Pichucalco
+', 5)
+INSERT INTO Municipios values (77,'Pijijiapan
+', 5)
+INSERT INTO Municipios values (78,'Pueblo Nuevo Solistahuacan
+', 5)
+INSERT INTO Municipios values (79,'Rayon
+', 5)
+INSERT INTO Municipios values (80,'Reforma
+', 5)
+INSERT INTO Municipios values (81,'Sabanilla
+', 5)
+INSERT INTO Municipios values (82,'Salto de Agua
+', 5)
+INSERT INTO Municipios values (83,'San Andres Duraznal
+', 5)
+INSERT INTO Municipios values (84,'San Cristobal de las Casas
+', 5)
+INSERT INTO Municipios values (85,'San Fernando
+', 5)
+INSERT INTO Municipios values (86,'San Juan Cancuc
+', 5)
+INSERT INTO Municipios values (87,'San Lucas
+', 5)
+INSERT INTO Municipios values (88,'Santiago el Pinar
+', 5)
+INSERT INTO Municipios values (89,'Siltepec
+', 5)
+INSERT INTO Municipios values (90,'Simojovel
+', 5)
+INSERT INTO Municipios values (91,'Sitala
+', 5)
+INSERT INTO Municipios values (92,'Socoltenango
+', 5)
+INSERT INTO Municipios values (93,'Solosuchiapa
+', 5)
+INSERT INTO Municipios values (94,'Soyalo
+', 5)
+INSERT INTO Municipios values (95,'Suchiapa
+', 5)
+INSERT INTO Municipios values (96,'Suchiate
+', 5)
+INSERT INTO Municipios values (97,'Sunuapa
+', 5)
+INSERT INTO Municipios values (98,'Tapachula
+', 5)
+INSERT INTO Municipios values (99,'Tapalapa
+', 5)
+INSERT INTO Municipios values (100,'Tapilula
+', 5)
+INSERT INTO Municipios values (101,'Tecpatan
+', 5)
+INSERT INTO Municipios values (102,'Tenejapa
+', 5)
+INSERT INTO Municipios values (103,'Teopisca
+', 5)
+INSERT INTO Municipios values (104,'Tila
+', 5)
+INSERT INTO Municipios values (105,'Tonala
+', 5)
+INSERT INTO Municipios values (106,'Totolapa
+', 5)
+INSERT INTO Municipios values (107,'Tumbala
+', 5)
+INSERT INTO Municipios values (108,'Tuxtla Chico
+', 5)
+INSERT INTO Municipios values (109,'Tuxtla Gutierrez
+', 5)
+INSERT INTO Municipios values (110,'Tuzantan
+', 5)
+INSERT INTO Municipios values (111,'Tzimol
+', 5)
+INSERT INTO Municipios values (112,'Union Juarez
+', 5)
+INSERT INTO Municipios values (113,'Venustiano Carranza
+', 5)
+INSERT INTO Municipios values (115,'Villa Comaltitlan
+', 5)
+INSERT INTO Municipios values (116,'Villa Corzo
+', 5)
+INSERT INTO Municipios values (117,'Villaflores
+', 5)
+INSERT INTO Municipios values (118,'Yajalon
+', 5)
+INSERT INTO Municipios values (119,'Zinacantan', 5)
+
+INSERT INTO Municipios values (120,'SoFarFarAway', 5)
+INSERT INTO Municipios values (121,'Nerverland', 5)
+INSERT INTO Municipios values (122,'Maincra', 5)
+INSERT INTO Municipios values (123,'Tartaros', 5)
+INSERT INTO Municipios values (124,'Skyrim', 5)
+
+Delete FROM Municipios WHERE Municipio_Nombre = 'SoFarFarAway'
+Delete FROM Municipios WHERE Municipio_Nombre = 'Nerverland'
+Delete FROM Municipios WHERE Municipio_Nombre = 'Maincra'
+Delete FROM Municipios WHERE Municipio_Nombre = 'Tartaros'
+Delete FROM Municipios WHERE Municipio_Nombre = 'Skyrim'
+
+SELECT * FROM Municipios
